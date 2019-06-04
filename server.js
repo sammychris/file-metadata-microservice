23b1fe078f416a7710a8bc21ebe7ae75b2a81e08
Samuel Christopher <Ebusameric@gmail.com>
2019-06-04 17:43:21 +0100

    complete the upload route with multer

--- server.js
+++ server.js
@@ -2,10 +2,14 @@
 
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' })
+
 
 // require and use "multer"...
 
 var app = express();
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -17,6 +21,15 @@ app.get('/', function (req, res) {
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+  let name = req.file.originalname;
+  let type = req.file.mimetype;
+  let size = req.file.size;
+  res.json({name, type, size})
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
